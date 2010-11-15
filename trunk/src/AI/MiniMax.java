c611cf9fc34d5417a6df08b925ac14b444823bc9
Lukaskang@gmail.com <Lukaskang@gmail.com@2dab68e9-0609-af9f-6faa-47f2a4e32115>
2010-11-15 11:37:18 +0000

    Hexyboard and Minimax, evaluation missing

---
+++ trunk/src/AI/MiniMax.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI;
+
+import Game.Board;
+import Game.HexyBoard;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lukas
+ */
+public class Minimax {
+
+    private MiniMaxTree tree;
+    private int maxDepth;
+
+    public Minimax(HexyBoard root, int d) {
+        tree = new MiniMaxTree(root);
+        maxDepth = d;
+    }
+
+    public int[] getNextMove(int PlayerID) {
+
+        for (int i = 0; i < maxDepth; i++) {
+            createNextLevel(PlayerID);
+            if (PlayerID == 1) {
+                PlayerID = 2;
+            } else {
+                PlayerID = 1;
+            }
+        }
+
+        //here comes the minimax evaluation or hexy evaluation
+        Node rooty = (Node) tree.root();
+        rooty.printTree();
+        return rooty.getChildren()[(int)(Math.random()*rooty.getChildren().length)].element().getMove();
+    }
+
+    public void createNextLevel(int PlayerID) {
+        Node root = (Node) tree.root();
+        if (root.getChildren() == null) {
+            root.expandNode(PlayerID);
+        } else {
+            ArrayList<Node> leaves = root.findLeaves();
+            for (int i = 0; i < leaves.size(); i++) {
+                leaves.get(i).expandNode(PlayerID);
+            }
+        }
+    }
+//    public boolean finished() {
+//        Node r = (Node) tree.root();
+//        return r.finished();
+//    }
+}
