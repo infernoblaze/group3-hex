9ac698cba3a0c0af43e3f63485d7949b74889883
jose.sue.pty <jose.sue.pty@2dab68e9-0609-af9f-6faa-47f2a4e32115>
2010-11-20 16:07:03 +0000

    And_Or evaluation is completed although it may be still buggy(not yet tested)

--- trunk/src/AI/And_Or.java
+++ trunk/src/AI/And_Or.java
@@ -5,29 +5,35 @@ import java.util.ArrayList;
 import Game.Board;
 
 /**
- * uses virtual connections to find best move
- * semi connection is better than connection
+ * uses virtual connections to determine how good a board is towards a player
  * @author Jose Sue Smith
- *
  */
 public class And_Or 
 {
-	Board board;
-	int player;
-	int[][] used;
-	int counter;
-	int size;
-	ArrayList<GroupCell> list;
+	private Board board;
+	private int player, counter, size, complete, semicomplete;
+	private int[][] used;
+	private ArrayList<GroupCell> list;
 	
+	/**
+	 * uses virtual connections to determine how good a board is towards a player
+	 * @param b board to be examined
+	 * @param p player for which it is examined
+	 */
 	public And_Or(Board b, int p)
 	{
 		board = b;
 		player = p;	
+		complete = 0;
+		semicomplete = 0;
 		size = board.getDimensions() * board.getDimensions();
 		used = new int [size][2];
 		counter = 0;
 	}
 	
+	/**
+	 * creates an Array List containing all groups of cells from a board
+	 */
 	public void groups()
 	{
 		list = new ArrayList<GroupCell>();
@@ -35,20 +41,49 @@ public class And_Or
 		for(int i = 0; i<size; i++)
 		{
 			GroupCell group = new GroupCell(board,player,used,counter);
-			if(group.found)
+			if(group.found())
 			{
 				list.add(group);
 				counter = group.getCounter();
 				used = group.usedList();
 			}
 		}
+	}	
+	
+	/**
+	 * determines if there is a virtual connection between 2 groups
+	 * @param a group of cells 1
+	 * @param b group of cells 2
+	 * @return 0 if there are no connections, 1 if connection is complete or 2 if its semi-complete
+	 */
+	public int type(GroupCell a, GroupCell b)
+	{		
+		VirtualConnection checker = new VirtualConnection(board,a,b);
+		return checker.check();
 	}
 	
-
-	public int type(ArrayList<GroupCell> a , ArrayList<GroupCell> b)
+	/**
+	 * evaluates the value of current board according to its virtual connections
+	 * @return	the value of current board
+	 */
+	public int evaluate()
 	{
-		VirtualConnection checker = new VirtualConnection(a,b);
-		return checker.check();
+		while(list.size()>0)
+		{
+			int value = type(list.get(0), list.get(1)); 
+			if(value == 1)
+			{
+				complete++;
+			}
+			if(value == 2)
+			{
+				semicomplete++;
+			}
+			list.remove(0);
+		}
+		int score = complete*3 + semicomplete;
+		
+		return score;
 	}
 	
 	
--- trunk/src/AI/VirtualConnection.java
+++ trunk/src/AI/VirtualConnection.java
@@ -1,27 +1,37 @@
 package AI;
 
-import java.util.ArrayList;
-
 import Game.Board;
+import Game.Board.Cell;
 
 /**
  * checks for virtual connections from group of cells
  * @author Jose Sue Smith
- *
  */
 public class VirtualConnection 
 {
-	Board board;
-	int player;
-	int[][] used;
-	int counter;
-	int size;
-	ArrayList<GroupCell> a, b;
+	private Board board;
+	private int[][] blanks1, blanks2, equals;
+	private int counter, counter1 , counter2;
+	private GroupCell a,b;
 	
-	public VirtualConnection(ArrayList<GroupCell> aList, ArrayList<GroupCell> bList)
+	/**
+	 * checks for virtual connections from 2 groups of cells
+	 * @param b1 board to which the groups belong
+	 * @param aGroup Group 1 to be examined
+	 * @param bGroup Group 2 to be examined
+	 */
+	public VirtualConnection(Board b1, GroupCell aGroup, GroupCell bGroup)
 	{
-		a = aList;
-		b = bList;
+		board = b1;
+		a = aGroup;
+		b = bGroup;
+		counter = 0;
+		counter1 = 0;
+		counter2 = 0;
+		blanks1 = new int[board.getDimensions()*board.getDimensions()][2];
+		blanks2 = new int[board.getDimensions()*board.getDimensions()][2];
+		equals = new int[board.getDimensions()*board.getDimensions()][2];
+		blankLists();
 	}
 	
 	/**
@@ -30,10 +40,95 @@ public class VirtualConnection
 	 */
 	public int check()
 	{
-		return 0;
+		for(int i = 0; i<counter1; i++)
+		{
+			for(int j = 0; j<counter2; j++)
+			{
+				if(blanks1[i][0] == blanks2[j][0])
+				{
+					equals[counter][0] = blanks1[i][0];
+					counter++;
+				}
+			}
+		}
+		if(counter == 0)
+		{
+			return 0;
+		}
+		
+		if(counter >2)
+		{
+			return 1;
+		}
+		
+		return 2;
 	}
 	
+	/**
+	 * checks if a cell has already been checked in a list
+	 * @param a cell to be checked in aList
+	 * @param aList list to be checked
+	 * @return true if it has not been checked, otherwise false
+	 */
+	public boolean unchecked(Cell a, int [][] aList)
+	{
+		for(int i = 0 ; i<aList.length; i++)
+		{
+			if(aList[i][0] == a.getX() && aList[i][1] == a.getY())
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 	
+	/**
+	 * creates list with blank cells from groups
+	 */
+	public void blankLists()
+	{
+		for(int i = 0; i<a.getCounter();i++)
+		{
+			Cell cell1 = board.getCell(a.group()[i][0], a.group()[i][1]);
+			Cell [] list1 = cell1.getNeighbours();
+			
+			for(int j = 0;j<list1.length;j++)
+			{
+				Cell aCell = list1[j];
+				if(aCell.getValue() == 0 && unchecked(aCell,blanks1))
+				{
+					blanks1[counter1][0] = aCell.getX();
+					blanks1[counter1][1] = aCell.getY();
+					counter1++;
+				}
+			}
+		}
+		
+		for(int i = 0; i<b.getCounter();i++)
+		{
+			Cell cell2 = board.getCell(b.group()[i][0], b.group()[i][1]);
+			Cell [] list2 = cell2.getNeighbours();
+			
+			for(int j = 0;j<list2.length;j++)
+			{
+				Cell bCell = list2[j];
+				if(bCell.getValue() == 0 && unchecked(bCell,blanks2))
+				{
+					blanks2[counter2][0] = bCell.getX();
+					blanks2[counter2][1] = bCell.getY();
+					counter2++;
+				}
+			}
+		}
+	}
 	
+	/**
+	 * List of cells that are bridges
+	 * @return List of cells that are bridges 
+	 */
+	public int[][] getEquals()
+	{
+		return equals;
+	}
 	
 }
