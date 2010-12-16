a119a9f96bdb449596c6245bf49ebb8d54ded46c
maarfijs <maarfijs@2dab68e9-0609-af9f-6faa-47f2a4e32115>
2010-12-16 10:10:50 +0000

    The real Monte Carlo and the old is renamed to RandomSimulationPlayer

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,5 +1,9 @@
 package Game;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import Players.*;
 import UI.BoardView;
 
@@ -30,7 +34,23 @@ public class Game implements Runnable {
 	 * Initializes a game and sets the board.
 	 */
 	public Game() {
-		board = new Board(5);
+		this(6, new HumanPlayer(), new MonteCarloPlayer());
+	}
+	
+	public Game(int boardSize, Player playerOne, Player playerTwo) {
+		board = new Board(boardSize);
+		
+		players = new Player[2];
+		
+		playerOne.setGame(this);
+		playerOne.setPlayerId(PLAYER_ONE);
+		players[0] = playerOne;
+		
+		playerTwo.setGame(this);
+		playerTwo.setPlayerId(PLAYER_TWO);
+		players[1] = playerTwo;
+		
+		endGameListenners = new ArrayList<ActionListener>();
 	}
 
 	/**
@@ -39,45 +59,66 @@ public class Game implements Runnable {
 	 */
 	public void run()
 	{
-		players = new Player[2];
-		players[0] = new MinimaxPlayer();
-		players[0].setGame(this);
-		players[0].setPlayerId(PLAYER_ONE);
-		players[1] = new HumanPlayer();
-		players[1].setGame(this);
-		players[1].setPlayerId(PLAYER_TWO);
-
 		playerTurn = PLAYER_ONE;
 		turn = 0;
 
 		this.doTurn();
 	}
+	
+	private int gameWinner;
 
+	/**
+	 * Carries out all the  operations for a turn to happen 
+	 */
 	private void doTurn() {
+		System.out.println("Turn: " + turn);
+		
+		// Lets get the active player
 		Player activePlayer = this.getPlayer(playerTurn);
 
-		boardView.waitingForMove(activePlayer);
+		// Lets inform board that we are waiting for a move 
+		if (boardView != null)
+			boardView.waitingForMove(activePlayer);
+		
+		// If it's the first turn the player can swap sides
 		if (turn == 1)
-			boardView.canSwapSides(true);
+		{
+			activePlayer.setCanSwapSides(true);
+			if (boardView != null)
+				boardView.setCanSwapSides(true);
+		}
 		else
-			boardView.canSwapSides(false);
-
+		{
+			activePlayer.setCanSwapSides(false);
+			if (boardView != null)
+				boardView.setCanSwapSides(false);
+		}
+				
+		// Lets ask the move from the player
 		int[] move = activePlayer.getNextMove();
 
+		// If it's the first turn of the game player might have wanted to swap
+		// the board.
 		if (turn == 1 && move[0] == -1 && move[1] == -1)
 			board.swapSides();
 		else
 			board.setPiece(move[0], move[1], activePlayer.getPlayerId());
 
-		boardView.repaint();
-
+		if (boardView != null)
+			boardView.repaint();
+		
+		// Lets check whether the game has ended
 		int endState = board.checkEnd();
 		if (endState != 0)
 		{
-			boardView.gameHasEnded(endState);
+			if (boardView != null)
+				boardView.gameHasEnded(endState);
+			gameWinner = endState;
+			performEndGameListeners();
 			return;
 		}
 
+		// Lets have a move from the other player
 		if (playerTurn == PLAYER_ONE)
 			playerTurn = PLAYER_TWO;
 		else if (playerTurn == PLAYER_TWO)
@@ -87,6 +128,11 @@ public class Game implements Runnable {
 		this.doTurn();
 	}
 
+	/**
+	 * Returns a player by its identifier
+	 * @param playerId numeric player identifier
+	 * @return the player
+	 */
     private Player getPlayer(int playerId)
 	{
 		for (Player player : this.players) {
@@ -96,7 +142,15 @@ public class Game implements Runnable {
 		}
 		return null;
 	}
+    
+    public int getGameWinner() {
+    	return gameWinner;
+    }
 
+    /**
+     * Sets the reference for the visual representation of the game.
+     * @param boardView reference to the board view
+     */
 	public void setBoardView(BoardView boardView)
 	{
 		this.boardView = boardView;
@@ -106,10 +160,28 @@ public class Game implements Runnable {
 
 	}
 
-	public Board getBoard() {
+	/**
+	 * Returns the game board at its current state
+	 * @return game board
+	 */
+	public Board getBoard()
+	{
 		return this.board;
 	}
 
+	private ArrayList<ActionListener> endGameListenners;
+	
+	public void addEndGameListener(ActionListener aListener)
+	{
+		endGameListenners.add(aListener);
+	}
+	
+	private void performEndGameListeners() {
+		for (ActionListener listener : endGameListenners) {
+			listener.actionPerformed(new ActionEvent(this, 1, "End Game"));
+		}
+	}
+	
 	/**
 	 * A constant that represents player one
 	 */
---
+++ src/Players/RandomSimulationPlayer.java
@@ -0,0 +1,150 @@
+package Players;
+
+import java.util.ArrayList;
+
+import Game.Board;
+import Game.Game;
+
+public class RandomSimulationPlayer implements Player
+{
+	
+	private Game game;
+	private int playerId;
+	
+	private int boardDimensions;
+	
+	private int[] branchingFactors;
+	
+	public RandomSimulationPlayer() {
+		this(new int[] { 36, 20, 10, 1});
+	}
+
+	public RandomSimulationPlayer(int[] factors) {
+		branchingFactors = factors;
+	}
+
+	public int[] getNextMove()
+	{
+		Board board = game.getBoard();
+		boardDimensions = board.getDimensions();
+		
+		TreeLeaf top = new TreeLeaf(board.clone());
+				
+		int otherPlayer = (playerId == Game.PLAYER_ONE ? Game.PLAYER_TWO : Game.PLAYER_ONE);
+		populateTree(top, top.board, otherPlayer, 1);
+		
+		int[] nextMove = {-2, -2};
+		double maxValue = Double.NEGATIVE_INFINITY;
+		
+		for (TreeLeaf leaf : top.children) {
+			int[] lastMove = leaf.board.getLastPiece();
+			
+			if (leaf.value > maxValue) {
+				nextMove = lastMove;
+				maxValue = leaf.value;
+			}
+		}
+		
+		return nextMove;
+	}
+	
+	private double populateTree(TreeLeaf leaf, Board board, int player, int depth)
+	{
+		
+		if (board.getDimensions() * 2 <= board.getPieceCount()) {
+			int endState = board.checkEnd();
+			if (endState != 0)
+				if (endState == playerId)
+					if (depth == 2)
+						return 100.0;
+					else
+						return 1.0 / depth;
+				else
+					return -1.0 / depth;
+		}
+				
+//		int branchCount = 1;
+//		if (depth < branchingFactors.length)
+//			branchCount = branchingFactors[depth - 1];
+		int branchCount = (depth == 1 ? 36 : depth == 2 ? 20 : depth == 3 ? 10 : 1);
+		
+		ArrayList<Integer[]> blankFields = new ArrayList<Integer[]>();
+		for (int i = 0; i < boardDimensions; i++)
+			for (int j = 0; j < boardDimensions; j++)
+				if (board.getField(i, j) == 0)
+				{
+					blankFields.add(new Integer[] {i, j});
+				}
+		
+		int otherPlayer = (player == Game.PLAYER_ONE ? Game.PLAYER_TWO : Game.PLAYER_ONE);
+		
+		double sum = 0.0;
+		
+		for (int i = 0; i < branchCount && !blankFields.isEmpty(); i++)
+		{
+			Board childBoard;
+			TreeLeaf childLeaf;
+			
+			if (branchCount == 1)
+				childBoard = board;
+			else
+				childBoard = board.clone();
+			
+			int r = (int) (Math.random() * blankFields.size());
+			Integer[] nextMove = blankFields.get(r);
+			blankFields.remove(r);
+			childBoard.setPiece(nextMove[0].intValue(), nextMove[1].intValue(), otherPlayer);
+			
+			if (leaf == null || depth > 3)
+				childLeaf = null;
+			else {
+				childLeaf = new TreeLeaf(childBoard);
+				childLeaf.parent = leaf;
+				leaf.children.add(childLeaf);
+			}
+			
+			double childSum = populateTree(childLeaf, childBoard, otherPlayer, depth + 1); 
+			
+			if (childLeaf != null)
+				childLeaf.value = childSum;
+			
+			sum = sum + childSum;
+		}
+		
+		if (leaf != null)
+			leaf.value = sum;
+		
+		return sum;
+	}
+
+	public void setGame(Game theGame)
+	{
+		this.game = theGame;
+	}
+	
+	public void setPlayerId(int thePlayer)
+	{
+		this.playerId = thePlayer;
+	}
+	
+	public int getPlayerId()
+	{
+		return this.playerId;
+	}
+	
+	private class TreeLeaf {
+		public TreeLeaf parent;
+		public ArrayList<TreeLeaf> children;
+		public Board board;
+		public double value;
+		
+		public TreeLeaf(Board board) {
+			children = new ArrayList<TreeLeaf>();
+			this.board = board;
+		}
+	}
+
+	public void setCanSwapSides(boolean state) {
+	}
+	
+}
